6916a9b231cccbeaf3a8d468f2c19f5f04aeb88e
Elpipi14 <107086659+Elpipi14@users.noreply.github.com>
2025-01-23 00:21:52 -0300

    portfolio

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -4,7 +4,7 @@ import myProjects from "./devPage";
 const Projects = () => {
   return (
     <section className="py-16 bg-gray-800 min-h-screen text-white">
-      <h2 className="text-center text-4xl font-extrabold mb-16 m-4 py-4 text-yellow-300">
+      <h2 className="text-center text-4xl font-extrabold mb-8 m-4 py-4 text-yellow-300">
         Mis Proyectos
       </h2>
       <div className="flex flex-col items-center justify-center m-2 p-2">
