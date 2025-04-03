60a512f7a71837eadda664fa188e711763dc8bc0
and Riss <ands_riss@hotmail.com>
2025-04-03 13:15:42 -0300

    portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ const payload = {
   token: captchaValue,
 };
 
-const response = await fetch(apiUrl, {
+await fetch(`${apiUrl}/api/contact`, {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify(payload),
