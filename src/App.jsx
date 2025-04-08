65cfc47306ae14cc8e68ac9428fc6eb840e77100
and Riss <ands_riss@hotmail.com>
2025-04-08 08:45:34 -0300

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './index.css';
 import Index from './pages/Index';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
 
   return (
     <>
       <Index />
+      <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            transition={Bounce}
+          />
     </>
   )
 }
